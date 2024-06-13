20b5b4d8e99c51729dbb9cf685ccdcee66e93037
M. Syahru Ramadhan <muhammadsyahru67@gmail.com>
2024-06-13 23:27:11 +0700

    first commit

--- firstly/lib/pages/page1.dart
+++ firstly/lib/pages/page1.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,37 +11,23 @@ class Page1 extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Spacer(flex: 2), // Spacer untuk memberikan jarak dari atas layar
-            Text(
-              '',
-              style: GoogleFonts.montserrat(
-                textStyle: TextStyle(
-                    fontSize: 30,
-                    color: Colors.green,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Spacer(
-                flex:
-                    9), // Spacer untuk memberikan sedikit jarak antara teks dan gambar
+            Spacer(flex: 2),
             Image.asset(
               'lib/gambar/Logo_Seulanga.png',
               height: 150,
             ),
-            Spacer(
-                flex:
-                    12), // Spacer untuk memberikan sedikit jarak antara logo dan teks baru
+            Spacer(flex: 1),
             Text(
               'Homepage',
               style: GoogleFonts.montserrat(
                 textStyle: TextStyle(
-                    fontSize: 22,
-                    color: const Color.fromARGB(255, 149, 148, 148),
-                    fontWeight: FontWeight.bold),
+                  fontSize: 22,
+                  color: Color.fromARGB(255, 149, 148, 148),
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            Spacer(
-                flex: 10), // Spacer untuk mengisi sisa ruang di bawah teks baru
+            Spacer(flex: 3),
           ],
         ),
       ),
